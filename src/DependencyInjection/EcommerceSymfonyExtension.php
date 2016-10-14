cec200b949b92c1128a3bfbc802be016d076aebf
sander <sander3@gmail.com>
2016-10-14 15:31:06 +0200

    Removed services import from functional tests config

--- src/DependencyInjection/EcommerceSymfonyExtension.php
+++ src/DependencyInjection/EcommerceSymfonyExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Milosa\EcommerceSymfony\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -12,8 +13,8 @@ class EcommerceSymfonyExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, __DIR__.'/../Resources/config/services');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/services'));
 
-        $loader->load('services.xml');
+        $loader->load('catalog.xml');
     }
 }
--- src/MilosaEcommerceSymfonyBundle.php
+++ src/MilosaEcommerceSymfonyBundle.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace Milosa\EcommerceSymfony;
 
+use Milosa\EcommerceSymfony\DependencyInjection\EcommerceSymfonyExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MilosaEcommerceSymfonyBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        if (null === $this->extension) {
+            $this->extension = new EcommerceSymfonyExtension();
+        }
+        if ($this->extension) {
+            return $this->extension;
+        }
+    }
 }
